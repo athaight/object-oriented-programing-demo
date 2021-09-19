b5994003f0735ebfe63fdd2926fce63d07c0ce50
Andrew Haight <haight.haight@gmail.com>
2021-09-19 13:18:12 -0700

    rethinking approach into seperate functions

--- index.js
+++ index.js
@@ -4,17 +4,14 @@ const inquirer = require('inquirer');
 const fs = require('fs');
 const Manager = require("./lib/Manager");
 const Engineer = require("./lib/engineer");
-const Employee = require("./lib/employee");
 const Intern = require("./lib/Intern");
+const Employee = require("./lib/employee");
 
 
 // WHEN I click on an email address in the HTML
 // THEN my default email program opens and populates the TO field of the email with the address
-
 // WHEN I click on the GitHub username
 // THEN that GitHub profile opens in a new tab
-
-
 // WHEN I enter the team manager’s name, employee ID, email address, and office number
 // THEN I am presented with a menu with the option to add an engineer or an intern or to finish building my team
 // WHEN I select the engineer option
@@ -23,10 +20,7 @@ const Intern = require("./lib/Intern");
 // THEN I am prompted to enter the intern’s name, ID, email, and school, and I am taken back to the menu
 // WHEN I decide to finish building my team
 // THEN I exit the application, and the HTML is generated
-
-
 // const { example } = require('yargs');
-
 // const generateHTML = (answers) =>
 //   `<!DOCTYPE html>
 // <html lang="en">
@@ -49,14 +43,13 @@ const Intern = require("./lib/Intern");
 //   </div>
 // </div>
 // </body>
-
 // </html>
+// `;
+// const generateEmployee = (answers) => `
+//     ${answers.employeeType}
 // `;
 
 
-const generateEmployee = (answers) => `
-    ${answers.employeeType}
-`;
 // WHEN I start the application
 // THEN I am prompted to enter the team manager’s name, employee ID, email address, and office number
 inquirer
@@ -64,8 +57,8 @@ inquirer
         {
             type: 'list',
             name: 'exployeeType',
-            message: "What would you like to do?",
-            choices: ["Create entire Team?", "Create Manager only?", "Create Employee only?" ],
+            message: "What role would you like to create for?",
+            choices: ["Create entire Team?", "Create Manager", "Create Engineer", "Create Intern" ],
             validate: (value) => {
                 if(value) {
                     return true;
@@ -76,94 +69,114 @@ inquirer
         },
     ])
 
-    .then((answers) => {
-        if(answers.employeeType == "Create entire Team?"){
-             inquirer
-           .prompt([
-               {
-                type: 'input',
-                name: 'managerName',
-                message: "What is the Managers's Name?",
-              },
-              {
-                type: 'input',
-                name: 'ManagerId',
-                message: "What is the Manager's Employee ID?",
-              },
-              {
-                type: 'input',
-                name: 'ManagerEmail',
-                message: "What is the Manager's Email?",
-              },
-              {
-                type: 'input',
-                name: 'officeNumber',
-                message: "What is the Managers's Office Number?",
-              },
-             {
-               type: 'input',
-               name: 'employeeName',
-               message: "What is the Employee's Name?",
-             },
-             {
-               type: 'input',
-               name: 'employeeId',
-               message: "What is the Employees's Employee ID?",
-             },
-             {
-               type: 'input',
-               name: 'employeeEmail',
-               message: "What is the Manager's Email?",
-             },
-             {
-               type: 'list',
-               name: 'engineerIntern',
-               message: "Is this an Engineer or and Intern?",
-               choices: ["Engineer", "Intern", "None"],
-               validate: (value) => {
-                   if (value) {
-                       return true;
-                   } else {
-                       return "Value is needed to continue.";
-                   }
-                }
-            }
-           ])
+function entireTeam(){
+inquirer
+.prompt([
+    {
+    type: 'input',
+    name: 'managerName',
+    message: "What is the Managers's Name?",
+  },
+  {
+    type: 'input',
+    name: 'ManagerId',
+    message: "What is the Manager's Employee ID?",
+  },
+  {
+    type: 'input',
+    name: 'ManagerEmail',
+    message: "What is the Manager's Email?",
+  },
+  {
+    type: 'input',
+    name: 'officeNumber',
+    message: "What is the Managers's Office Number?",
+  },
+  {
+    type: 'input',
+    name: 'employeeName',
+    message: "What is the Employee's Name?",
+  },
+  {
+    type: 'input',
+    name: 'employeeId',
+    message: "What is the Employees's Employee ID?",
+  },
+  {
+    type: 'input',
+    name: 'employeeEmail',
+    message: "What is the Manager's Email?",
+  },
+  {
+    type: 'list',
+    name: 'engineerIntern',
+    message: "Is this an Engineer or and Intern?",
+    choices: ["Engineer", "Intern", "None"],
+    validate: (value) => {
+        if (value) {
+            return true;
+        } else {
+            return "Value is needed to continue.";
+        }
+    }
+}
+])
 
-           .then((answers) => {
-            if(answers.engineerIntern === "None"){
-            const teamData = generateTeam(answers);
-            fs.writeFile("index.html", teamData, (err) =>
-            err ? console.log(err) : console.log("Sucessfully created manager.html"))
-            } else if (answers.engineerIntern === "Engineer"){
-              return inquirer 
-              .prompt([
-              {
-                type: 'input',
-                name: 'github',
-                message: "What is the Engineer's GitHub?",
-              },
-              {
-                type: 'list',
-                name: 'Intern',
-                message: "Is there an Intern to add?",
-                choices: ["Intern", "None"],
-                validate: (value) => {
-                   if (value) {
-                       return true;
-                   } else {
-                       return "Value is needed to continue.";
-                   }
-                }
-              },
-            ])
+function manager(){
+  inquirer 
+    .prompt([
+    {
+      type: 'input',
+      name: 'github',
+      message: "What is the Engineer's GitHub?",
+    },
+  ])
+  }
+
+function engineer(){
+inquirer 
+  .prompt([
+  {
+    type: 'input',
+    name: 'github',
+    message: "What is the Engineer's GitHub?",
+  },
+])
+}
+
+function intern(){
+  inquirer
+  .prompt([
+{
+  type: 'list',
+  name: 'Intern',
+  message: "Is there an Intern to add?",
+  choices: ["Intern", "None"],
+  default: 'Intern',
+},
+{
+  type: 'input',
+  name: 'school',
+  message: "What school?"
+},
+])
+}
+}
+
+          //  .then((answers) => {
+          //   if(answers.engineerIntern === "None"){
+          //   const teamData = generateTeam(answers);
+          //   fs.writeFile("index.html", teamData, (err) =>
+          //   err ? console.log(err) : console.log("Sucessfully created manager.html"))
+          //   } else if (answers.engineerIntern === "Engineer"){
+          //     return 
         
-              .then((answers) => {
-                if(answers.Intern === "None"){
-                const teamDataNoIntern = generateTeam(answers);
-                fs.writeFile("index.html", teamDataNoIntern, (err) =>
-                err ? console.log(err) : console.log("Sucessfully created inedx.html"))
+          //     .then((answers) => {
+          //       if(answers.Intern === "None"){
+          //       const teamDataNoIntern = generateTeam(answers);
+          //       fs.writeFile("index.html", teamDataNoIntern, (err) =>
+          //       err ? console.log(err) : console.log("Sucessfully created inedx.html"))
              
-          } else {
-              return console.log("Ya gotta do something man, can't just dit there...");
-          }})}})}});
+          // } else {
+          //     return console.log("Ya gotta do something man, can't just dit there...");
+          // }})}})}})
